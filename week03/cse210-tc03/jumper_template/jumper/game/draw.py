7e392f6bf26952d68088e1a06bed43e68d5c8f7e
McKay Williams <wmsmckay@205-185-96-190.directcom.com>
2021-01-23 20:00:59 -0700

    added comments and updated readme

--- week03/cse210-tc03/jumper_template/jumper/game/draw.py
+++ week03/cse210-tc03/jumper_template/jumper/game/draw.py
@@ -1,14 +1,45 @@
 class Draw:
+    """
+    A code template for the jumper of the game. This class's responsibility
+    is to calculate changes to the jumper and then display them to the
+    console.
+
+    Stereotype:
+        Information Holder
+
+    Attributes:
+        jumper (list): A list with the "image" of the jumper.
+    """
 
     def __init__(self):
+        """
+        Class constructor. Declares and initializes instance attributes.
+        
+        Args:
+            self (Draw): An instance of Draw.
+        """
         self.jumper = ["  ___  "," /___\ "," \   / ","  \ /  ","   0   ", "  /|\  ","  / \  ","        ","^^^^^^^"]
 
 
     def drawJumper(self):
+        """
+        Prints out the jumper.
+
+        Args:
+            self (Draw): An instance of Draw.
+        """
         for x in self.jumper:
             print(x)
 
     def updateJumper(self, guess):
+        """
+        Updates the condition of the jumper's parachute.
+
+        Args:
+            self (Draw): An instance of Draw.
+            guess (boolean): Whether the player's guess was good.
+        """
+
         if guess:
             pass
         else:
@@ -16,6 +47,12 @@ class Draw:
 
 
     def deadJumper(self):
+        """
+        Prints out the jumper if the player loses  the game.
+
+        Args:
+            self (Draw): An instance of Draw
+        """
         
         self.jumper = ["   x   ", "  /|\  ","  / \  ","        ","^^^^^^^"]
         self.drawJumper()
--- week03/cse210-tc03/jumper_template/jumper/game/readfile.py
+++ week03/cse210-tc03/jumper_template/jumper/game/readfile.py
@@ -1,11 +1,34 @@
 
 class ReadFile:
+    """
+    A template for reading in a file. The contets are added to a list
+    for the game to expand it's possible words.
+
+    Stereotype:
+        Service Provider	
+    
+    Attributes:
+        wordList (list): A list of the words from the file.
+    """
 
     def __init__ (self):
+        """
+        Class constructor. Declares and initializes instance attributes.
+        
+        Args:
+            self (ReadFile): An instance of ReadFile.
+        """
         self.wordList = self.createList()
 
 
     def createList(self):
+        """
+        Reads the contents of a specific file and puts the contents into
+        a list seperated by commas. That list is returned.
+
+        Args:
+            self (ReadFile): An instance of ReadFile.
+        """
         textFile = open("jumper/game/sampleWords.txt", "r")
         content = textFile.read()
         myList = content.split(", ")
